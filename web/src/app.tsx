68a18d47dffe2375d7777a331efcf400cb11dc25
anielqsilva <anielqsilva@gmail.com>
2024-10-24 23:54:37 +0000

    feat: adding fetch to backend's api

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,30 @@
 import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
+import { useState } from "react";
 // import { EmptyGoals } from "./components/empty-goals";
 
 export function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  fetch('http://localhost:3333/summary').then(response => {
+    response.json().then(data => {
+      console.log(data)
+    })
+  })
+
   return (
     <Dialog>
+      <button onClick={increment} type="button">Incrementar</button>
+      <h1 className="text-4xl">{count}</h1>
+
       {/* <EmptyGoals /> */}
 
-      <Summary />
+      {/* <Summary /> */}
       <CreateGoal />
     </Dialog>
   );
